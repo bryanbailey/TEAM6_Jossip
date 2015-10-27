7a6e6e2121ed6018a623950c8896ee47b2a23ba0
Praveen Kumar G <praveen.govindaraj07@gmail.com>
2015-10-27 12:20:14 -0400

    added test message

--- browsecos.php
+++ browsecos.php
@@ -5,63 +5,84 @@
     <title>Jossip company rankings page</title>
 
     <link rel="stylesheet" type="text/css" href="./resources/css/jossstyle.css" />
+    <script src="./vendors/jquery-2.1.4.min.js"></script>
+
+    <!-- Bootstrap 3.3.5 JS, Bootstrap 3.3.5 CSS-->
+    <script src="./vendors/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./vendors/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./resources/css/jgStyle.css">
+
+    <!-- General Job Gossip styling -->
+
 
 </head>
 <body>
+<?php
+session_start();                    //call at very begining of all pages
 
-<div id = "container">
+include './resources/php/navbar.php';
+?>
 
-    <div id = "content">
+<div class = "container">
+    <div class="jumbotron">
+        <h1>Jossip!</h1>
+        <p>Some words here. <a href="#">A Link.</a></p>
+    </div>
 
-        <div id = "header">
-            <h1>Jossip</h1>
-        </div>
 
-        <div id = "nav">
+    <!-- <div id = "header">
+        <h1>Jossip</h1>
+    </div> -->
 
-            <ul>
-                <li><a class="selected" href="index.php">Home</a></li><br>
-                <li><a class="selected" href="register.php">New user?<br>Create an<br>account by<br>clicking here</a></li><br>
-            </ul>
+    <div id = "nav">
 
-        </div>
-
-<!-- Again, the following division is just mocked up for appearance, needs to be hooked to the database to produce the live
-results we are looking for -bb -->
-
-        <div id = "main">
-            <h3>Current company rankings</h3>
-            <?php
-            $companies = array (
-                array ("IBM", 4.5),
-                array ("Wells Fargo", 3),
-                array ("Bank of America", 3),
-                array ("Swisher", 2),
-            );
-
-            for ($row = 0; $row <  4; $row++) {
-                echo "<br><br><b>";
-                echo $row+1;
-                echo ". ";
-                echo $companies[$row][0]."</b><br>";
-                echo "Rating: ".$companies[$row][1];
-            }
-              ?>
-        </div>
-
-<!-- This, again, needs to be relevant to whether the user is logged in or not. -bb -->
-
-        <div id = "logout">
-
-            <form action="login.php">
-                <input type="submit" value="Login">
-            </form>
-
-        </div>
+        <!-- <ul>
+            <li><a class="selected" href="index.php">Home</a></li><br>
+            <li><a class="selected" href="register.php">New user?<br>Create an<br>account by<br>clicking here</a></li><br>
+        </ul> -->
 
     </div>
 
+    <!-- Again, the following division is just mocked up for appearance, needs to be hooked to the database to produce the live
+    results we are looking for -bb -->
+
+    <div class="col-sm-3">
+        <div class="list-group">
+            <a href="#">Post Jobs</a>
+        </div>
+    </div>
+    <div id = "main">
+        <h3>Current Top Trending company rankings</h3>
+        <?php
+        $companies = array (
+            array ("IBM", 4.5),
+            array ("Wells Fargo", 3),
+            array ("Bank of America", 3),
+            array ("Swisher", 2),
+        );
+
+        for ($row = 0; $row <  4; $row++) {
+            echo "<br><br><b>";
+            echo $row+1;
+            echo ". ";
+            echo $companies[$row][0]."</b><br>";
+            echo "Rating: ".$companies[$row][1];
+        }
+        ?>
+    </div>
+
+    <!-- This, again, needs to be relevant to whether the user is logged in or not. -bb -->
+
+    <!-- <div id = "logout">
+
+        <form action="login.php">
+            <input type="submit" value="Login">
+        </form>
+
+    </div> -->
+
 </div>
+
 
 </body>
 </html>
