58e48a3aae121b37fe4e726abb9bc6341ae509a0
Praveen Kumar G <praveen.govindaraj07@gmail.com>
2015-10-27 12:22:28 -0400

    added test message

--- loginlanding.php
+++ loginlanding.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,43 +6,61 @@
     <title>Jossip login landing page</title>
 
     <link rel="stylesheet" type="text/css" href="./resources/css/jossstyle.css" />
+    <!-- jquery 2.1.4 -->
+    <script src="./vendors/jquery-2.1.4.min.js"></script>
+
+    <!-- Bootstrap 3.3.5 JS, Bootstrap 3.3.5 CSS-->
+    <script src="./vendors/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./vendors/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./resources/css/jgStyle.css">
+
+    <!-- General Job Gossip styling -->
 
 </head>
 <body>
 
-<div id = "container">
+<?php
+session_start();                    //call at very begining of all pages
 
-    <div id = "content">
+include './resources/php/navbar.php';
+?>
+<div class = "container">
 
-        <div id = "header">
-            <h1>Jossip</h1>
-        </div>
 
-        <div id = "nav">
+    <!-- <div id = "header">
+        <h1>Jossip</h1>
+    </div> -->
+    <div class="jumbotron">
+        <h1>Jossip!</h1>
+        <p>Some words here. <a href="#">A Link.</a></p>
+    </div>
 
-            <ul>
-                <li><a class="selected" href="index.php">Home</a></li><br>
-            </ul>
+    <div id = "nav">
 
-        </div>
+        <!-- <ul>
+            <li><a class="selected" href="index.php">Home</a></li><br>
+        </ul> -->
 
-        <div id = "main">
-            <h3>Welcome to Jossip</h3>
-            <p>You've successfully created an account with Jossip!</p><br>
-            <p>Would you like to:</p>
-            <ol>
-                <li><a href="newaccount.php">Change your account details?</a></li>
-                <li><a href="postrate.php">Post and rate a position/company?</a></li>
-            </ol>
+    </div>
 
-        </div>
-        <div id = "login">
+    <div id = "main">
+        <h3>Welcome to Jossip</h3>
+        <p>You've successfully created an account with Jossip!</p><br>
+        <p>Would you like to:</p>
+        <ol>
+            <li><a href=#>Change your account details?</a></li>
+            <li><a href="browsecos.php">Post and rate a position/company?</a></li>
+        </ol>
 
-            <form action="index.php">
-                <input type="submit" value="Logout">
-            </form>
+    </div>
 
-        </div>
+    <div id = "login">
+
+        <!-- <form action="index.php">
+            <input class="btn btn-primary" type="submit" value="Logout">
+        </form> -->
+
+    </div>
 </div>
 
 </body>
