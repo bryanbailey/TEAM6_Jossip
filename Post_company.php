b60337d72b92831e245fb5dc7ffbef0b77dd2ec1
bryanbailey <bbaile25@uncc.edu>
2015-10-27 21:09:13 -0400

    cleaned up page

--- Post_company.php
+++ Post_company.php
@@ -22,7 +22,7 @@ if( isset($_POST['post_company'])) {
     <meta name="author" content="Praveen Kumar G">
     <title>New User Registration</title>
     <!-- jquery 2.1.4 -->
-    <script src="/vendors/jquery-2.1.4.min.js"></script>
+    <script src="./vendors/jquery-2.1.4.min.js"></script>
     <!-- Bootstrap 3.3.5 core JS, Bootstrap 3.3.5 CSS-->
     <script src="./vendors/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="./vendors/bootstrap-3.3.5-dist/css/bootstrap.min.css">
@@ -34,7 +34,7 @@ if( isset($_POST['post_company'])) {
 </head>
 <body>
 <?php
-include '/resources/php/navbar.php';
+include './resources/php/navbar.php';
 ?>
 
 
@@ -81,19 +81,18 @@ if( isset($_POST['post_company'])) {
                     </div>
                     <label for="rating">Rating</label>
                     <span class="starRating">
-                        <input id="rating5" type="radio" name="rating" value="5">
-                        <label for="rating5">5</label>
-                        <input id="rating4" type="radio" name="rating" value="4">
-                        <label for="rating4">4</label>
-                        <input id="rating3" type="radio" name="rating" value="3">
-                        <label for="rating3">3</label>
-                        <input id="rating2" type="radio" name="rating" value="2">
-                        <label for="rating2">2</label>
                         <input id="rating1" type="radio" name="rating" value="1">
                         <label for="rating1">1</label>
-                        </span>
-
-                    <br />
+                        <input id="rating2" type="radio" name="rating" value="2">
+                        <label for="rating2">2</label>
+                        <input id="rating3" type="radio" name="rating" value="3">
+                        <label for="rating3">3</label>
+                        <input id="rating4" type="radio" name="rating" value="4">
+                        <label for="rating4">4</label>
+                        <input id="rating5" type="radio" name="rating" value="5">
+                        <label for="rating5">5</label>
+                    </span>
+                    <br /><br />
                     <div class="form-group">
                         <button type="submit" class="form-control btn btn-primary btn-block" name="post_company" >Post</button>
                     </div>
