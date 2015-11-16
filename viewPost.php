5c3ce96a1bc5dc7ca9cfa90085bcbb9ba7b4f436
bryanbailey <bbaile25@uncc.edu>
2015-11-16 12:17:08 -0500

    updated by Bryan

--- viewPost.php
+++ viewPost.php
@@ -41,7 +41,7 @@ include '/resources/php/navbar.php';
 <div class = "container">
 
 
-    <h1 class="page-header">(Post results from existing posters)</h1>
+    <h1 class="page-header">Jossip Post</h1>
 
     <div class="col-sm-3">
         <?php
@@ -52,30 +52,33 @@ include '/resources/php/navbar.php';
     <div class = "col-sm-9">
         <?php
 
-        while( $post = $postListSQLQuery->fetch_assoc() ){
+    $post = $postListSQLQuery->fetch_assoc();
             echo '
                 <div class="panel panel-default">
                     <div class="panel-heading">Company Name : <b>',$post['company_name'],'</b></div>
 
-                    <div class="panel-body"><b>Position Title : </b>',$post['position_title'],'</b></span>
+                        <div class="panel-body"><b>Position Title : </b>',$post['position_title'],'</b></span>
 
-                        <div class="panel-body"><b>Nature of work invlolved:</b>
+                            <div class="panel-body"><b>Nature of work invlolved:</b>
                              ',$post['post_content'],'</span>
-                        </div>
-                    <div class="panel-heading" style="font-size:small"><i></b>(Poster: <b>',$post['first_name'],')</b>
-<span class ="pull-right"> Please rate the Post  <div class="rating">
+                            </div>
+                        <div class="panel-heading" style="font-size:small"><i></b>Poster: <b>',$post['first_name'],'</i></b>
+                    </div>
+                </div>
+    </div>
 
-      <input type="radio" id="star5" name="jobrating" value="5" /><label for="star5" title="Best">5 stars</label>
-      <input type="radio" id="star4" name="jobrating" value="4" /><label for="star4" title="Pretty good">4 stars</label>
-      <input type="radio" id="star3" name="jobrating" value="3" /><label for="star3" title="Satisfactory">3 stars</label>
-      <input type="radio" id="star2" name="jobrating" value="2" /><label for="star2" title="Not great">2 stars</label>
-      <input type="radio" id="star1" name="jobrating" value="1" /><label for="star1" title="Unsatisfactory">1 star</label>
-      </div>
-  </span>
-                    </div>
-                    </div>
-             ';
-        }
+    <br>
+
+        <span"> Please rate the in its helpfulness to you:  <div class="rating"><br><br>
+          <input type="radio" id="star5" name="jobrating" value="5" /><label for="star5" title="Best">5 stars</label>
+          <input type="radio" id="star4" name="jobrating" value="4" /><label for="star4" title="Pretty good">4 stars</label>
+          <input type="radio" id="star3" name="jobrating" value="3" /><label for="star3" title="Satisfactory">3 stars</label>
+          <input type="radio" id="star2" name="jobrating" value="2" /><label for="star2" title="Not great">2 stars</label>
+          <input type="radio" id="star1" name="jobrating" value="1" /><label for="star1" title="Unsatisfactory">1 star</label>
+        </span>
+
+    ';
+
 
         ?>
     </div>
